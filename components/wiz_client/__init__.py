7e885d43c02573efc0d0f0213c686327c97b0347
David Friedland <david.friedland@abovelending.com>
2025-04-19 04:27:16 +0000

    Refactor WizClient initialization and configuration schema for improved clarity and structure

--- components/wiz_client/__init__.py
+++ components/wiz_client/__init__.py
@@ -1,15 +1,42 @@
+# components/wiz_client/__init__.py
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import component
+from esphome.const import CONF_ID
 
-# Create a namespace for the Wiz component.
-wiz_ns = cg.esphome_ns.namespace('wiz_client')
-WizClient = wiz_ns.class_('WizClient', cg.Component)
+# Configuration keys
+CONF_BULBS = 'bulbs'
+CONF_INITIAL_BRIGHTNESS = 'initial_brightness'
+CONF_INITIAL_COLOR_TEMPERATURE = 'initial_temp'
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.Optional('name'): cv.string,
-}).extend(component.COMPONENT_SCHEMA.schema)
+# Create a namespace matching your C++ namespace (wiz_client)
+wiz_client_ns = cg.esphome_ns.namespace('wiz_client')
+# Declare the C++ class
+WizClient = wiz_client_ns.class_('WizClient', cg.Component)
 
-def to_code(config):
-    var = cg.new_Pvariable(config.get('name', 'wiz_client'), WizClient)
-    cg.add(var.setup())
+# Define the schema for the wiz_client: YAML block
+definition = cv.Schema({
+    cv.GenerateID(): cv.declare_id(WizClient),
+    cv.Required(CONF_BULBS): cv.All(
+        cv.ensure_list(cv.string),
+        cv.Length(min=1)
+    ),
+    cv.Optional(CONF_INITIAL_BRIGHTNESS, default=100): cv.int_range(0, 100),
+    cv.Optional(CONF_INITIAL_COLOR_TEMPERATURE, default=2700): cv.int_range(1700, 6500),
+})
+
+# Extend the base component schema so ESPHome can recognize
+CONFIG_SCHEMA = definition.extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    # Instantiate the C++ object
+    var = cg.new_Pvariable(config[CONF_ID])
+    # Register with the component system (calls setup())
+    await cg.register_component(var, config)
+
+    # Add each bulb IP
+    for ip_str in config[CONF_BULBS]:
+        cg.add(var.add_bulb(ip_str))
+
+    # Set initial brightness and color temperature
+    cg.add(var.set_brightness(config[CONF_INITIAL_BRIGHTNESS]))
+    cg.add(var.set_color_temperature(config[CONF_INITIAL_COLOR_TEMPERATURE]))
