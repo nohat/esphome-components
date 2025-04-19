000c973ae737246c75140fc9d3b8ded9ed645bc9
David Friedland <david.friedland@abovelending.com>
2025-04-19 17:31:00 +0000

    Refactor WizClient header by removing redundant comments and adjusting namespace placement for clarity

--- components/wiz_client/wiz_client.h
+++ components/wiz_client/wiz_client.h
@@ -4,9 +4,10 @@
 #include <WiFiUdp.h>
 #include <vector>
 
+namespace wiz_client {
+
 class WizClient : public Component {
  public:
-  // Initialize with optional starting brightness (0-100)
   WizClient() : brightness_(100) {}
 
   void setup() override {
@@ -14,7 +15,6 @@ class WizClient : public Component {
     udp_.begin(38899);
   }
 
-  // Add a bulb to control by IP ("192.168.1.42")
   void add_bulb(const char* ip_str) {
     IPAddress ip;
     if (ip.fromString(ip_str)) {
@@ -25,7 +25,6 @@ class WizClient : public Component {
     }
   }
 
-  // Set global brightness (0-100)
   void set_brightness(int brightness) {
     brightness_ = std::max(0, std::min(brightness, 100));
     char buf[128];
@@ -35,7 +34,6 @@ class WizClient : public Component {
     send_udp(buf, len);
   }
 
-  // Set color temperature in Kelvin (1700-6500)
   void set_color_temperature(int temp) {
     temp = std::max(1700, std::min(temp, 6500));
     char buf[128];
@@ -46,7 +44,6 @@ class WizClient : public Component {
   }
 
  protected:
-  // Make send_udp virtual so that it can be overridden in tests.
   virtual void send_udp(const char* data, size_t len) {
     for (auto &ip : ips_) {
       udp_.beginPacket(ip, 38899);
@@ -60,3 +57,5 @@ class WizClient : public Component {
   std::vector<IPAddress> ips_;
   int brightness_;
 };
+
+} // namespace wiz_client
