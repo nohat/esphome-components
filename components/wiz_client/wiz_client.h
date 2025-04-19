5c45138c0b130debbe40a742a4574c640d6b0462
David Friedland <david.friedland@abovelending.com>
2025-04-19 18:02:31 +0000

    Fix incorrect variable reference in set_brightness method

--- components/wiz_client/wiz_client.h
+++ components/wiz_client/wiz_client.h
@@ -1,62 +1,71 @@
 #pragma once
-#include "esphome/core/component.h"  // Use the correct Component definition
-#include "esphome/core/log.h"        // For ESP_LOGD and ESP_LOGW
+#include "esphome/core/component.h" // Use the correct Component definition
+#include "esphome/core/log.h"       // For ESP_LOGD and ESP_LOGW
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <vector>
 
-namespace wiz_client {
+namespace wiz_client
+{
 
-class WizClient : public esphome::Component {
- public:
-  WizClient() : brightness_(100) {}
+  class WizClient : public esphome::Component
+  {
+  public:
+    WizClient() : brightness_(100) {}
 
-  void setup() override {
-    ESP_LOGD("wiz", "Initializing WizClient UDP on port 38899");
-    udp_.begin(38899);
-  }
-
-  void add_bulb(const char* ip_str) {
-    IPAddress ip;
-    if (ip.fromString(ip_str)) {
-      ips_.push_back(ip);
-      ESP_LOGD("wiz", "Added Wiz bulb %s", ip_str);
-    } else {
-      ESP_LOGW("wiz", "Invalid IP for Wiz bulb: %s", ip_str);
+    void setup() override
+    {
+      ESP_LOGD("wiz", "Initializing WizClient UDP on port 38899");
+      udp_.begin(38899);
     }
-  }
 
-  void set_brightness(int brightness) {
-    brightness_ = std::max(0, std::min(brightness, 100));
-    char buf[128];
-    int len = snprintf(buf, sizeof(buf),
-      "{\"method\":\"setPilot\",\"params\":{\"dimming\":%d}}", pct);
-      brightness_);
-    send_udp(buf, len);
-  }
-
-  void set_color_temperature(int temp) {
-    temp = std::max(1700, std::min(temp, 6500));
-    char buf[128];
-    int len = snprintf(buf, sizeof(buf),
-      "{\"method\":\"setPilot\",\"params\":{\"mode\":\"white\",\"temp\":%d,\"dimming\":%d}}",
-      temp, brightness_);
-    send_udp(buf, len);
-  }
-
- protected:
-  virtual void send_udp(const char* data, size_t len) {
-    for (auto &ip : ips_) {
-      udp_.beginPacket(ip, 38899);
-      udp_.write((const uint8_t*)data, len);
-      udp_.endPacket();
-      ESP_LOGD("wiz", "Sent UDP to %s: %s", ip.toString().c_str(), data);
+    void add_bulb(const char *ip_str)
+    {
+      IPAddress ip;
+      if (ip.fromString(ip_str))
+      {
+        ips_.push_back(ip);
+        ESP_LOGD("wiz", "Added Wiz bulb %s", ip_str);
+      }
+      else
+      {
+        ESP_LOGW("wiz", "Invalid IP for Wiz bulb: %s", ip_str);
+      }
     }
-  }
 
-  WiFiUDP udp_;
-  std::vector<IPAddress> ips_;
-  int brightness_;
-};
+    void set_brightness(int brightness)
+    {
+      brightness_ = std::max(0, std::min(brightness, 100));
+      char buf[128];
+      int len = snprintf(buf, sizeof(buf), "{\"method\":\"setPilot\",\"params\":{\"dimming\":%d}}", brightness_);
+      send_udp(buf, len);
+    }
+
+    void set_color_temperature(int temp)
+    {
+      temp = std::max(1700, std::min(temp, 6500));
+      char buf[128];
+      int len = snprintf(buf, sizeof(buf),
+                         "{\"method\":\"setPilot\",\"params\":{\"mode\":\"white\",\"temp\":%d,\"dimming\":%d}}",
+                         temp, brightness_);
+      send_udp(buf, len);
+    }
+
+  protected:
+    virtual void send_udp(const char *data, size_t len)
+    {
+      for (auto &ip : ips_)
+      {
+        udp_.beginPacket(ip, 38899);
+        udp_.write((const uint8_t *)data, len);
+        udp_.endPacket();
+        ESP_LOGD("wiz", "Sent UDP to %s: %s", ip.toString().c_str(), data);
+      }
+    }
+
+    WiFiUDP udp_;
+    std::vector<IPAddress> ips_;
+    int brightness_;
+  };
 
 } // namespace wiz_client
